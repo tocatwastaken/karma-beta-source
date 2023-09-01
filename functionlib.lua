d9b2ab5c6602d74a15fe85aad47c5e121cdb8d99
ThatRobloxDev <59939720+bakersrule2020@users.noreply.github.com>
2023-09-01 09:05:48 -0400

    Updated FunctionLib

--- functionlib.lua
+++ functionlib.lua
@@ -11,4 +11,36 @@ _G["RBXUtils"] = {
         repeat wait() until game:IsLoaded()
         functoconnect()
     end,
+    OnCharacterAdded = function(functoconnect)
+        OnPlayerJoined(function(player)
+            connection = player.CharacterAdded:Connect(functoconnect)
+            return connection
+        end)
+    end,
+    NewInstance = function(typetomake, properties)
+        inst = Instance.new(typetomake, properties)
+        return inst
+    end,
+    DestroyInstance = function(insttodestroy)
+        insttodestroy:Destroy()
+    end,
+    ConsoleClear = function()
+        rconsoleclear()
+    end,
+    ConsoleTitle = function(title)
+        rconsoletitle(title)
+    end,
+    ConsolePrint = function(msg)
+        rconsoleprint(msg .. "\n")
+    end,
+    ConsoleWarn = function(msg)
+        rconsoleprint("[WARN]", msg .. "\n")
+    end,
+    ConsoleErr = function(msg)
+        rconsoleprint("[ERROR]", msg .. "\n")
+    end, 
+    ConsoleInfo = function(msg)
+        rconsoleprint("[INFO]", msg .. "\n")
+    end,
+    
 }
